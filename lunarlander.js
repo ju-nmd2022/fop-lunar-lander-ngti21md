16350dfc3759a83d8b5550567115f7086df6ccaa
ngti21md <117225303+ngti21md@users.noreply.github.com>
2023-03-04 14:31:03 +0100

    Update lunarlander.js

--- lunarlander.js
+++ lunarlander.js
@@ -1,13 +1,36 @@
-const px = 300,
-      py = 100;
+let x = 300,
+    y = 100,
+    gravity = 1;
+let p = 5, //position
+    v, //speed
+    h = height,
+    g,
+    fuel = 60,
+    engine;      
 
 function setup() {
-  createCanvas (600, 900);
+  createCanvas (600, 700);
   noLoop();
 }
 function draw() {
+  keyPressed ();
   space();
   moon();
+  fuelFunc ();
+  
+}
+
+function keyPressed (){
+  if (keyCode == 'ENTER') {
+    start ();
+  }
+}
+
+function start(){
+  fill(255, 255, 255);
+  textAlign(CENTER);
+  textSize(60);
+  text("Lunar Lander", x, y + 20);  
 }
 
 function space() {
@@ -23,14 +46,44 @@ function space() {
 }
 function moon() {
   fill (160, 160, 160);
-  rect (0, height * 0.75, width, height * 0.25);
+  rect (0, h * 0.75, width, h * 0.25);
 
   push ();
+  translate (50, 500);
   fill (160, 160, 160);
-  Stroke ();
-  ellipse (300, 300, 80, 20);
+  ellipse (0, 0, 80, 20);
   pop ();
 } 
+ function ship() {
+  // translate (x, y);
+  rect (x, y, 40); 
+}
+
+function landing() {
+  if (key == ' ') {
+    fuel -= 2;
+    p = 2;
+    ship.rect.y -= v;
+  }
+  else {
+    p = 5;
+  }
+}
+
+function fuelFunc () {
+  textSize (30);
+  text ("Fuel: " + fuel , x - 280, y - 50);
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
